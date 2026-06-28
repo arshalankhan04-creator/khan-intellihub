7cdb73c2674259a503c8965811f3537c65435af6
arshalankhan04-creator <arshalankhan04@gmail.com>
2026-06-28 15:21:22 +0530

    fix: update settings allowed CORS origins to include port 5174

--- khan_intellihub/settings/base.py
+++ khan_intellihub/settings/base.py
@@ -176,7 +176,7 @@ SIMPLE_JWT = {
 # ---------------------------------------------------------------------------
 # CORS (Req 10.2)
 # ---------------------------------------------------------------------------
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:5173', cast=Csv())
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174', cast=Csv())
 CORS_ALLOW_CREDENTIALS = True
 
 # ---------------------------------------------------------------------------
